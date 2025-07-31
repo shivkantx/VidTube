e4e09fbb2d1a418da0ab7df785319e94885ef997
Shiv Kant <shivkant639624@gmail.com>
2025-07-31 23:25:15 +0530

    Healthcheck routes and testing with postman

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT || 8001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port : ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection error ", err);
+  });
---
+++ src/routes/healthcheck.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { healthcheck } from "../controllers/healthcheck.controller.js";
+
+const router = Router();
+// /api/v1/healthcheck/test
+router.route("/").get(healthcheck);
+router.route("/test").get(healthcheck);
+export default router;
