198ffa45b141567c1b034e520267290dc87606a0
Shiv Kant <shivkant639624@gmail.com>
2025-08-20 23:47:28 +0530

    All tweets  opertion done

--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -7,18 +7,94 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
   //TODO: create tweet
+  const { content } = req.body;
+  const userId = req.user._id;
+
+  if (!content || content.trim() === "") {
+    throw new ApiError(400, "Content is required for tweet");
+  }
+
+  const tweet = await Tweet.create({
+    owner: userId,
+    content,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, tweet, "Tweet created successfully"));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
-  // TODO: get user tweets
+  const { userId } = req.params;
+  const loggedInUserId = req.user._id;
+
+  // If no userId is passed, default to logged-in user
+  const targetUserId = userId || loggedInUserId;
+
+  if (!mongoose.Types.ObjectId.isValid(targetUserId)) {
+    throw new ApiError(400, "Invalid userId");
+  }
+
+  const tweets = await Tweet.find({ owner: targetUserId })
+    .sort({ createdAt: -1 })
+    .populate("owner", "username email");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "User tweets fetched successfully"));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
-  //TODO: update tweet
+  const { content } = req.body;
+  const { tweetId } = req.params;
+  const userId = req.user._id;
+
+  if (!content || content.trim() === "") {
+    throw new ApiError(400, "Content is required for tweet");
+  }
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweetId");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+
+  if (tweet.owner.toString() !== userId.toString()) {
+    throw new ApiError(403, "You are not allowed to update this tweet");
+  }
+
+  tweet.content = content;
+  await tweet.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet updated successfully"));
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
-  //TODO: delete tweet
+  const userId = req.user._id;
+  const { tweetId } = req.params;
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweetid");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+
+  if (tweet.owner.toString() !== userId.toString()) {
+    throw new ApiError(403, "You are not allowed to delete this tweet");
+  }
+
+  await Tweet.findByIdAndDelete(tweetId);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Tweet deleted successfully"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
--- src/models/tweet.models.js
+++ src/models/tweet.models.js
@@ -1,8 +1,8 @@
 /** id string pk
-  owner ObjectId users
-  content string
-  createdAt Date
-  updatedAt Date */
+  owner ObjectId users   -> the user who posted the tweet
+  content string         -> the text content of the tweet
+  createdAt Date         -> when it was created
+  updatedAt Date         -> last update timestamp */
 
 import mongoose, { Schema } from "mongoose";
 
